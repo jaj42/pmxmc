f2eda812b5461b0cdc4de4b4cfee32e43a2bd20a
Jona JOACHIM <jona@joachim.cc>
2026-04-16 16:25:48 +0200

    update ode parameters

--- pmxmc/advan/ode.py
+++ pmxmc/advan/ode.py
@@ -6,10 +6,9 @@ from pmxmc.utils import build_rate_func
 
 @wrap_jax
 def ode_advan(meas_time, infu_time, infu_rate, pk_ode, params, y0):
-    y0 = jnp.asarray(y0)
     ode_term = dfx.ODETerm(pk_ode)
     solver = dfx.Tsit5()
-    pid_controller = dfx.PIDController(rtol=1e-6, atol=1e-8)
+    pid_controller = dfx.PIDController(rtol=1e-7, atol=1e-9)
     stepsize_controller = dfx.ClipStepSizeController(pid_controller, jump_ts=infu_time)
     max_steps = 100_000
     saveat = dfx.SaveAt(ts=meas_time)
@@ -22,8 +21,8 @@ def ode_advan(meas_time, infu_time, infu_rate, pk_ode, params, y0):
         solver=solver,
         t0=tbeg,
         t1=tend,
-        y0=y0,
-        dt0=None,
+        y0=jnp.asarray(y0),
+        dt0=meas_time[1] - meas_time[0],
         stepsize_controller=stepsize_controller,
         max_steps=max_steps,
         saveat=saveat,
